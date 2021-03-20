f6fa97fa4af3f293741ad27cb6790d7ff0177b32
unknown <chris@nextgm.ca>
2021-03-20 14:28:15 +0100

    cleanup

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <template>
-
-  <component :is="this.$route.meta.layout || 'div'">
+  <component :is="$route.meta.layout || 'div'">
     <router-view />
   </component>
 </template>
 
 <script>
-
 export default {
   name: "App",
   // ...
